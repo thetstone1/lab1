a7e9c2a488211fbda5e309018a6174da54cba683
thetstone1 <m186252@usna.edu>
2018-02-01 10:06:13 -0500

    minor edit lab1

--- rgb_controller/rgb.py
+++ rgb_controller/rgb.py
@@ -2,6 +2,7 @@
 
 import serial
 import pygame, sys
+import time
 from pygame.locals import *
 
 pygame.init()
@@ -10,16 +11,23 @@ DISPLAYSURF.fill((255,255,255))
 pygame.display.set_caption('Lab 1 Stone')
 s = serial.Serial("/dev/ttyACM0")
 
-while(True):
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
-    l = s.readline()
-    x = l.rstrip().lstrip().split(",")
-    rgb = [int(val) for val in x]
-    print rgb
-    DISPLAYSURF.fill(rgb)    
-    pygame.display.update()
+def main():
+  while(True):
+    for event in pygame.event.get():
+      if event.type == QUIT:
+        pygame.quit()
+        sys.exit()
+      l = s.readline()
+      x = l.rstrip().lstrip().split(",")
+      try:
+        rgb = [int(val) for val in x]
+        DISPLAYSURF.fill(rgb)    
+        pygame.display.update()
+        print rgb
+      except ValueError:
+        pass
+     
+
+main()
 
 
