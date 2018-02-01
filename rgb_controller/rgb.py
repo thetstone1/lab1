e2e95f6a5d0e0ed1fef8125fa413dba4a104922a
thetstone1 <m186252@usna.edu>
2018-02-01 10:40:17 -0500

    minor change. Aligned try/catch for consistent function

--- rgb_controller/rgb.py
+++ rgb_controller/rgb.py
@@ -2,12 +2,11 @@
 
 import serial
 import pygame, sys
-import time
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((1366,768))
-DISPLAYSURF.fill((255,255,255))
+
+DISPLAYSURF = pygame.display.set_mode((400,400))
 pygame.display.set_caption('Lab 1 Stone')
 s = serial.Serial("/dev/ttyACM0")
 
@@ -17,17 +16,16 @@ def main():
       if event.type == QUIT:
         pygame.quit()
         sys.exit()
+    try:
       l = s.readline()
-      x = l.rstrip().lstrip().split(",")
-      try:
-        rgb = [int(val) for val in x]
-        DISPLAYSURF.fill(rgb)    
-        pygame.display.update()
-        print rgb
-      except ValueError:
-        pass
-     
-
+      x = l.rstrip().split(",")
+      color_r = int(x[0])
+      color_g = int(x[1])
+      color_b = int(x[2])
+      DISPLAYSURF.fill((color_r,color_g,color_b))    
+      pygame.display.update()
+    except ValueError:
+      pass
 main()
 
 
